39b5e64fa1982ad394c813d39b3c5ccb293ce2f8
Tbsn <tbn@propelcom.dk>
2024-01-23 10:36:38 +0100

    Add Nuxt.js static files

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    baseURL: '/nuxt3-tailwind-api/'
+   
+  },
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+
+  },
+})
+
---
+++ pages/data.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>API products</h1>
+  <div class="grid grid-cols-4  gap-5">
+    <div v-for="p in products">
+            {{p.title}}
+    </div>
+  </div>
+</template>
+
+
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+})
+
+const { data: products } = await useFetch('https://jsonplaceholder.typicode.com/posts')
+</script>
