aa6fcf49fe2c8f10138a8d6f146eb59855501e9e
Tbsn <tbn@propelcom.dk>
2024-01-23 10:52:46 +0100

    Add Nuxt.js static files 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/nuxt3-tailwind-api/'
+    baseURL: '/nuxt3-tailwind-api/dist'
    
   },
   devtools: { enabled: true },
