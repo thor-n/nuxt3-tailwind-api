da9aa6aa29fc20a9a65c95e81906689fec80350b
Tbsn <tbn@propelcom.dk>
2024-01-23 11:42:28 +0100

    Add Nuxt.js static files 4

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,7 +32,7 @@
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="index" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
+            <a href="index.html" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
             <a href="data" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">API data</a>
             
           </div>
